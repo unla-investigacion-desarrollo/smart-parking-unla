86b95c57aafde2a6fe0d6d53603f89025185b950
patricioarango <patricioarango@gmail.com>
2025-04-07 11:41:35 -0300

    delete passwords

--- Esp32-code/esp32v1/env.h
+++ Esp32-code/esp32v1/env.h
@@ -4,15 +4,15 @@
 //CONFIGURACION ID SENSOR TIPO SERIAL FROM BACKOFFICE
 const char *SENSOR_ID = "idSerial_CAXDVDFV";
 // CONFIGURACION WIFI
-const char *WIFI_SSID = "ArangoWifi6";
-const char *WIFI_PASSWORD = "SanLORENZO$014v2025";
+const char *WIFI_SSID = "";
+const char *WIFI_PASSWORD = "";
 
 //CONFIGURACION MQTT BROKER
 const char *MQTT_BROKER_HOST = "206.189.237.167";
 const int MQTT_BROKER_PORT = 1883;
 const char *MQTT_BROKER_TOPIC = "testtopic/sensors";
-const char *MQTT_BROKER_USERNAME = "patricio23";
-const char *MQTT_BROKER_PASSWORD = "entrar123";
+const char *MQTT_BROKER_USERNAME = "";
+const char *MQTT_BROKER_PASSWORD = "";
 
 
 #endif // CONF_H
