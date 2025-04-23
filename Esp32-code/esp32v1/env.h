bcc6f39c2dfe4162c664355d78f10697fc4a9734
patricioarango <patricioarango@gmail.com>
2025-04-23 15:11:25 -0300

    add nestjs server

--- Esp32-code/esp32v1/env.h
+++ Esp32-code/esp32v1/env.h
@@ -4,15 +4,16 @@
 //CONFIGURACION ID SENSOR TIPO SERIAL FROM BACKOFFICE
 const char *SENSOR_ID = "idSerial_CAXDVDFV";
 // CONFIGURACION WIFI
-const char *WIFI_SSID = "";
-const char *WIFI_PASSWORD = "";
+const char *WIFI_SSID = "ArangoWifi6";
+const char *WIFI_PASSWORD = "SanLORENZO$014v2025";
 
 //CONFIGURACION MQTT BROKER
 const char *MQTT_BROKER_HOST = "206.189.237.167";
 const int MQTT_BROKER_PORT = 1883;
 const char *MQTT_BROKER_TOPIC = "testtopic/sensors";
-const char *MQTT_BROKER_USERNAME = "";
-const char *MQTT_BROKER_PASSWORD = "";
+const char *MQTT_BROKER_USERNAME = "patricio23";
+const char *MQTT_BROKER_PASSWORD = "entrar123";
+const int TIME_DELAY = 5000;
 
 
 #endif // CONF_H
