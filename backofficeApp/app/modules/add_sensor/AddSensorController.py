d246073a83d82a96d1ad74a0a14b20f444f3996d
patricioarango <patricioarango@gmail.com>
2025-07-25 15:25:27 -0300

    remove files

--- backofficeApp/app/modules/add_sensor/AddSensorController.py
+++
@@ -1,3 +0,0 @@
-from . import add_sensor_blueprint
-import os,json
-from app.models import SensorData,Sensor
--- backofficeApp/app/modules/add_sensor/__init__.py
+++
@@ -1,4 +0,0 @@
-from flask import Blueprint
-add_sensor_blueprint = Blueprint("add_sensor", __name__,static_folder='files')
-
-from . import AddSensorController
