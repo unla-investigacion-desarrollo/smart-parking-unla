cb17877b17db471a73b24f7c9d1efcd21f9dea9d
patricioarango <patricioarango@gmail.com>
2025-08-18 18:03:57 -0300

    fix card colors

--- SmartParkingMobileApp/app/components/HomeCard.jsx
+++ SmartParkingMobileApp/app/components/HomeCard.jsx
@@ -1,27 +1,43 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
-import { Avatar, Button, Card, Text } from 'react-native-paper';
+import { Avatar, Card, Icon, Text } from 'react-native-paper';
 
-const LeftContent = props => <Avatar.Icon {...props} icon="map-marker" />
+const IconoLibre = props => <Avatar.Icon {...props} icon="map-marker" color='#00e676' backgroundColor='transparent' size="60"/>
+const IconoOcupado = props => <Avatar.Icon {...props} icon="map-marker" color='#ff1744' backgroundColor='transparent' size="60"/>
+
+const GreenFlag = () => (
+  <Icon
+    source="flag"
+    color="#00e676"
+    size={20}
+  />
+);
+const RedFlag = () => (
+  <Icon
+    source="flag"
+    color="#ff1744"
+    size={20}
+  />
+);
 
 export default function MyCardHome({sensor,index}){
 
   return(
     <Card key={sensor.id} style={styles.container}>
-    <Card.Title title={"Slot id " + sensor.id + " Name " + sensor.name } subtitle={" Slot Group " + sensor.parking_slot_group_id} left={LeftContent} />
+    <Card.Title title={"Slot id " + sensor.id + " Name " + sensor.name } subtitle={" Slot Group " + sensor.parking_slot_group_id} left={sensor.free ? IconoLibre : IconoOcupado} />
     <Card.Cover source={{ uri: 'http://129.212.182.8:5005/static/images/est'+ index +'.png' }} />
     <Card.Content>
       <Text variant="bodyMedium">Sensor ID: {sensor.sensor_id}</Text>
-      <Text variant="bodyMedium">is free: {sensor.free}</Text>
+      <Text variant="bodyMedium">is free: {sensor.free} {sensor.free ? <GreenFlag/> : <RedFlag/>}</Text>
       <Text variant="bodyMedium">status: {sensor.status}</Text>
       <Text variant="bodyMedium">distance: {sensor.distance}</Text>
       <Text variant="bodyMedium">last updated: {new Date(sensor.updated_at).toLocaleString()}</Text>
     </Card.Content>
     
-    <Card.Actions>
+    {/*<Card.Actions>
       <Button>Cancel</Button>
       <Button>Ok</Button>
-    </Card.Actions>
+    </Card.Actions>*/}
   </Card>
 )};
 
